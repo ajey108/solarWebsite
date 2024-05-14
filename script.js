44a50cbfed79e9e6b1c7519ce050f2e071e8520c
ajey108 <ajayxa1@gmail.com>
2024-05-14 13:57:45 +0530

    removed testimonal section

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-    new Splide('#testimonial-slider', {
-        type: 'fade',
-        perPage: 1,
-        autoplay: true,
-        pauseOnHover: false,
-        interval: 4000 // 4 seconds interval
-    }).mount();
-});
+
 
 // Toggle mobile menu visibility
 const hamburgerBtn = document.getElementById('hamburger');
